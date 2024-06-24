9596271dbba8f9ec4acf608e9cccfce82534e6f0
Gerson Fabian Buenahora Ormaza <138251681+bueorm@users.noreply.github.com>
2024-06-24 16:05:06 -0500

    Delete Ember/predict.py

--- Ember/predict.py
+++
@@ -1,4 +0,0 @@
-def predict(client, text, image_path=None):
-    if client.model is None:
-        raise ValueError("Model not loaded. Use load_model to load a model first.")
-    return client.model.predict(text, image_path)
